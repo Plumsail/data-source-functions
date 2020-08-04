248792b372145e4d5ba98f0181321244f636e8ec
Anna Zabolotskaya <a.zabolotskaya@plumsail.com>
2020-08-04 22:11:15 +0300

    filter items by Location

--- FunctionApp/SharePoint/ListData.cs
+++ FunctionApp/SharePoint/ListData.cs
@@ -1,28 +1,22 @@
-using System;
-using System.IO;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Microsoft.Graph;
-using Microsoft.Identity.Client;
-using System.Net.Http.Headers;
-using Microsoft.Graph.Auth;
-using System.Linq;
-using System.Collections.Generic;
 using Microsoft.Extensions.Options;
+using Microsoft.Graph;
+using Plumsail.DataSource.SharePoint.Settings;
 
 namespace Plumsail.DataSource.SharePoint
 {
     public class ListData
     {
-        private readonly Settings.ListData _settings;
         private readonly GraphServiceClientProvider _graphProvider;
+        private readonly Settings.ListData _settings;
 
-        public ListData(IOptions<Settings.AppSettings> settings, GraphServiceClientProvider graphProvider)
+        public ListData(IOptions<AppSettings> settings, GraphServiceClientProvider graphProvider)
         {
             _settings = settings.Value.ListData;
             _graphProvider = graphProvider;
@@ -30,7 +24,8 @@ namespace Plumsail.DataSource.SharePoint
 
         [FunctionName("SharePoint-ListData")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]
+            HttpRequest req,
             ILogger log)
         {
             log.LogInformation("ListData is requested.");
@@ -38,10 +33,11 @@ namespace Plumsail.DataSource.SharePoint
             var graph = _graphProvider.Create();
             var list = await graph.GetListAsync(_settings.SiteUrl, _settings.ListName);
 
-            var queryOptions = new List<QueryOption>()
+            var queryOptions = new List<QueryOption>
             {
                 new QueryOption("select", "id"),
-                new QueryOption("expand", "fields(select=Title,Author)")
+                new QueryOption("filter", $"fields/Title eq {req.Query["Location"]}"),
+                new QueryOption("expand", "fields(select=Title,Author,Location)")
             };
             var itemsPage = await list.Items
                 .Request(queryOptions)
